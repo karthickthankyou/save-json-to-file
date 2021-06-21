f461422d597b9db5c092bf0e18a40ecf1d1f122b
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-06-21 14:06:33 +0530

    added flatten support

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 #!/usr/bin / env node
 const chalk = require('chalk')
 const fs = require('fs')
+const { stringify } = require('flatted')
 
 // const [inputObject] = process.argv.slice(2)
 // const [exportPath] = process.argv.slice(3)
 
 console.clear()
 
-module.exports.saveJsonToFile = function (inputObject, exportPath) {
-  var jsonContent = JSON.stringify(inputObject)
+module.exports.saveJsonToFile = function (
+  inputObject,
+  exportPath,
+  flatten = false,
+) {
+  let stringifyFunc = flatten ? stringify : JSON.stringify
+  var jsonContent = stringifyFunc(inputObject)
 
   fs.writeFile(exportPath, jsonContent, 'utf8', function (err) {
     if (err) {
@@ -22,7 +28,9 @@ module.exports.saveJsonToFile = function (inputObject, exportPath) {
 
     console.log(
       chalk.black.bgBlueBright.bold(
-        ` JSON file has been saved in ${exportPath} `,
+        `${
+          flatten ? ' Flattened' : ''
+        } JSON file has been saved in ${exportPath} `,
       ),
     )
   })
--- test.js
+++ test.js
@@ -2,5 +2,11 @@ const { saveJsonToFile } = require('./index')
 
 saveJsonToFile(
   { name: 'Karthick', occupation: 'Software Developer', hobbies: 'Writing' },
+  'output2.json',
+)
+
+saveJsonToFile(
+  { name: 'Karthick', occupation: 'Software Developer', hobbies: 'Writing' },
   'output.json',
+  true,
 )
